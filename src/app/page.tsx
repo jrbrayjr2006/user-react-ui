3fcb45d6ffb1f5a98594a024628846bab754eca7
James Bray <jrbrayjr@gmail.com>
2024-11-05 20:00:04 -0600

    add login form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,21 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  let isInvalid = false;
   return (
     <main>
       <h1>User Login</h1>
+      <form>
+        <label>
+          Username:
+          <input className={ isInvalid ? 'invalid' : undefined} type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </main>
   );
 }
